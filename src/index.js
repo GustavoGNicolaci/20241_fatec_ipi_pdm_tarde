89b48f1665e2778775b98717664f7b9267b331c4
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2024-02-23 18:07:37 -0300

    feat(props): exibe pedidos do cliente

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const App = () => {
+    return (
+        <div className="container border rounded mt-2">
+
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="display-5 text-center">Seus pedidos</h1>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-12 col-lg-4">
+                    <div className="card mb-2">
+                        <div className="card-header text-muted">
+                            20/04/2023
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className='d-flex align-items-center'>
+                                <i className="fa-solid fa-house fa-2x"></i>
+                            </div>
+                            <div className='flex-grow-1 ms-2 border p-2'>
+                                <h4 className="text-center">Livro</h4>
+                                <p className='text-center'>Concrete Mathematics - Donald Knuth</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-lg-4">
+                    <div className="card mb-2">
+                        <div className="card-header text-muted">
+                            20/04/2023
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className='d-flex align-items-center'>
+                                <i className="fa-solid fa-expand fa-2x"></i>
+                            </div>
+                            <div className='flex-grow-1 ms-2 border p-2'>
+                                <h4 className="text-center">SSD</h4>
+                                <p className='text-center'>SSD 120Gb</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-lg-4">
+                    <div className="card mb-2">
+                        <div className="card-header text-muted">
+                            20/04/2023
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className='d-flex align-items-center'>
+                                <i className="fa-solid fa-sd-card fa-2x"></i>
+                            </div>
+                            <div className='flex-grow-1 ms-2 border p-2'>
+                                <h4 className="text-center">Notebook</h4>
+                                <p className='text-center'>Notebook Dell - i5 16Gb</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
