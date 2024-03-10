408956cff2dc012b6cf895865b4b4e85ec2843b5
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2024-03-10 12:52:03 -0300

    feat(ap05): fim da secao 2.9
    - implementa funcao de obtencao de localizacao
    - implementa JSX, com alguns detalhes faltando
    - lida com potenciais erros de maneira simplificada e diferente da apostila

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -11,11 +11,12 @@ class App extends React.Component {
             longitude: null,
             estacao: null,
             data: null,
-            icone: null
+            icone: null,
+            erro: null
         }
     }
 
-    obterEstacao = (latitude, longitude) => {
+    obterEstacao = (data, latitude) => {
         const anoAtual = data.getFullYear()
         //21/6
         const d1 = new Date(anoAtual, 5, 21)
@@ -38,6 +39,37 @@ class App extends React.Component {
         return estouNoSul ? 'Outono' : 'Primavera'
     }
 
+    obterLocalizacao = () => {
+        //usando a api de geolocalizacao, extrair a localizacao do usuario
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //quando estiver disponivel:
+                //extrair a data atual do sistema
+                let dataAtual = new Date()
+                //encontrar a estacao climatica usando a data e o local
+                let estacao = this.obterEstacao(dataAtual, position.coords.latitude)
+                //acessar o mapa de icones e pegar aquele que esta associado a estacao detectada
+                let icone = this.icones[estacao]
+                //atualizar o estado    
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    estacao: estacao,
+                    data: dataAtual.toLocaleTimeString(),
+                    icone: icone
+                })
+            },
+            (erro) => {
+                //quando houver erro:
+                console.log(erro)
+                this.setState({
+                    erro: 'Por favor, permita o acesso a sua localização.'
+                })
+
+            }
+        )
+    }
+
     icones = {
         'Primavera': 'fa-seedling',
         'Verão': 'fa-sun',
@@ -47,7 +79,38 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>Meu app</div>
+            <div className="container mt-4">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-8">
+                        <div className="card">
+                            {/* corpo */}
+                            <div className="card-body">
+                                <div style={{height: '6rem'}} className="ps-2 d-flex align-items-center border rounded mb-2">
+                                    <i className={`fa-solid fa-5x ${this.state.icone}`}></i>
+                                    <p className="w-75 ms-3 text-center fs-1">{this.state.estacao}</p>
+                                </div>
+                                <div>
+                                    <p className="text-center mt-3">
+                                        {
+                                            this.state.latitude
+                                            ? `Coords: ${this.state.latitude}, ${this.state.longitude}. Data: ${this.state.data}`
+                                            : 'Clique no botão para saber sua estação climática.'
+                                        }
+                                    </p>
+                                    <p className="text-center text-danger">
+                                        {this.state.erro}
+                                    </p>
+                                </div>
+                                <div>
+                                    <button className='btn btn-outline-primary w-100 mt-2' onClick={this.obterLocalizacao}>
+                                        Qual a minha estação?
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
