5a02cd9e5b2180dcf8c9080ec0bc02f0a0dfae1b
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2024-04-05 18:20:23 -0300

    feat(ap07): conduz a secao 2.10
    - faz requisição direcionada a pexels usando seu sdk
    - há um exercicio proposto para fazer uso de back end

--- busca-fotos/src/components/App.js
+++ busca-fotos/src/components/App.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import Busca from './Busca'
 import env from 'react-dotenv'
+import { createClient } from 'pexels'
 
 export default class App extends React.Component {
-    onBuscaRealizada = (termoDaBusca) => {
-        console.log(termoDaBusca)
+
+    pexelsClient = null
+    onBuscaRealizada = (termo) => {
+        this.pexelsClient.photos.search({ query: termo, per_page: 2 }).then((photos) => {
+            console.log(photos)
+        })
+    }
+
+    componentDidMount() {
+        this.pexelsClient = createClient(env.PEXELS_KEY)
     }
 
     render() {
-        console.log(env.PEXELS_KEY)
-        console.log(window.env.PEXELS_KEY)
         return (
             <div className="grid border-round border-1 border-400 justify-content-center w-9 m-auto">
                 <div className="col-12">
                     <h1>Exibir uma lista de...</h1>
                 </div>
                 <div className="col-8">
-                    <Busca 
+                    <Busca
                         onBuscaRealizada={this.onBuscaRealizada}
                     />
                 </div>
