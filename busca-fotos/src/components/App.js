bde46db947509d20d1ca030bb9807adb89298375
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2024-04-26 16:11:13 -0300

    feat(ap07): encerra a secao 2.12
    - exibe o logo da Pexels

--- busca-fotos/src/components/App.js
+++ busca-fotos/src/components/App.js
@@ -3,6 +3,7 @@ import Busca from './Busca'
 import env from 'react-dotenv'
 import { createClient } from 'pexels'
 import ListaImagens from './ListaImagens'
+import PexelsLogo from './PexelsLogo'
 
 export default class App extends React.Component {
 
@@ -26,6 +27,9 @@ export default class App extends React.Component {
     render() {
         return (
             <div className="grid border-round border-1 border-400 justify-content-center w-9 m-auto">
+                <div className="col-12">
+                    <PexelsLogo />
+                </div>
                 <div className="col-12">
                     <h1>Exibir uma lista de...</h1>
                 </div>
---
+++ busca-fotos/src/components/PexelsLogo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PexelsLogo = () => {
+    return (
+        <div>
+            <a href="https://www.pexels.com" target='_blank'>
+                <img width={75} src="https://images.pexels.com/lib/api/pexels.png"
+                />
+            </a>
+        </div>
+    )
+}
+
+export default PexelsLogo
