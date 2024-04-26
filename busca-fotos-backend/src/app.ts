25dd05257f82b6747e27ad34b0c3dbd8bc8de6a3
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2024-04-26 18:17:27 -0300

    feat(back): faz busca na pexels
    - define um endpoint /search que recebe termo e per_page do front e conversa com a pexels usando a axios

--- busca-fotos-backend/src/app.ts
+++ busca-fotos-backend/src/app.ts
@@ -3,9 +3,32 @@ dotenv.config();
 import express from 'express';
 import axios from 'axios';
 
-const { PORT } = process.env;
+const { PORT, PEXELS_KEY } = process.env;
 
 const app = express();
 app.use(express.json());
+
+app.get("/search", async (req, res) => {
+    const client = axios.create({
+        baseURL: "https://api.pexels.com/v1",
+        headers: {
+            Authorization: PEXELS_KEY
+        }
+    })
+
+    const result = await client.get("/search", {
+        params: {
+            query: req.query.termo,
+            per_page: req.query.per_page
+        }
+    });
+
+    res.json(result.data) 
+
+});
+
+app.get("/hello", (req, res) => {
+    res.send("Hello!");
+});
 
 app.listen(PORT, () => console.log(`Executando na porta ${PORT}`));
